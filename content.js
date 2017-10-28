bbb96400221b0227433fe99184d286e9d04be4d7
richardroy <richardroygibson@gmail.com>
2017-10-28 15:27:22 +1100

    moved classes to consts

--- content.js
+++ content.js
@@ -19,46 +19,56 @@ function runChanges() {
     //enter here the action you want to do once loaded
     console.log("runChanges");
 
+    //classes exist?
+    const fileSummaryRowClass = "iterable-item file file-modified"    
+    const commitFilesSummaryId = "commit-files-summary";
+    const diffStatsDivClass = "commit-file-diff-stats";
+    const linesRemovedClass = "lines-removed";
+    const linesAddedClass = "lines-added";    
+
+    const summaryElement = window.document.getElementById(commitFilesSummaryId);
+    if(summaryElement){
+
+        var removed  = summaryElement.getElementsByClassName(linesRemovedClass);
+        var totalRemoved = 0;
+        for (var i = 0; i < removed.length; i++) {
+            totalRemoved += parseInt(removed[i].innerText);
+        };
     
-    var removed  = window.document.getElementsByClassName("lines-removed");
-    var totalRemoved = 0;
-    for (var i = 0; i < removed.length; i++) {
-        totalRemoved += parseInt(removed[i].innerText);
-    };
+        var added  = summaryElement.getElementsByClassName(linesAddedClass);
+        var totalAdded = 0;
+        for (var i = 0; i < added.length; i++) {
+            totalAdded += parseInt(added[i].innerText);
+        };
 
-    var added  = window.document.getElementsByClassName("lines-added");
-    var totalAdded = 0;
-    for (var i = 0; i < added.length; i++) {
-        totalAdded += parseInt(added[i].innerText);
-    };
+        var li = document.createElement("li");
+        li.className = fileSummaryRowClass;
     
-    var li = document.createElement("li");
-    li.className = "iterable-item file file-modified";
-
-    var div = document.createElement("div");
-    div.className = "commit-file-diff-stats";
-
-    var spanAdded = document.createElement("span");
-    spanAdded.className = "lines-added";
-    spanAdded.textContent = totalAdded;
-
-    var spanRemoved = document.createElement("span");
-    spanRemoved.className = "lines-removed";
-    spanRemoved.textContent = totalRemoved;
-
-    var totalDif = totalAdded + totalRemoved;
-    var totalClass = totalDif >= 0 ? "lines-added" : "lines-removed";
-
-    var spanTotal = document.createElement("span");
-    spanTotal.className = totalClass;
-    spanTotal.textContent = totalDif;
-
-    li.appendChild(div);
-    div.appendChild(spanAdded);
-    div.appendChild(spanRemoved);
-    div.appendChild(spanTotal);
-
-    var difSummary = document.getElementsByClassName("commit-files-summary")[0];
-    if(!difSummary) runChanges();
-    difSummary.appendChild(li);
+        var div = document.createElement("div");
+        div.className = diffStatsDivClass;
+    
+        var spanAdded = document.createElement("span");
+        spanAdded.className = linesAddedClass;
+        spanAdded.textContent = totalAdded;
+    
+        var spanRemoved = document.createElement("span");
+        spanRemoved.className = linesRemovedClass;
+        spanRemoved.textContent = totalRemoved;
+    
+        var totalDif = totalAdded + totalRemoved;
+        var totalClass = totalDif >= 0 ? linesAddedClass : linesRemovedClass;
+    
+        var spanTotal = document.createElement("span");
+        spanTotal.className = totalClass;
+        spanTotal.textContent = totalDif;
+    
+        li.appendChild(div);
+        div.appendChild(spanAdded);
+        div.appendChild(spanRemoved);
+        div.appendChild(spanTotal);
+    
+        summaryElement.appendChild(li);
+    } else {
+        console.warn("Delay: No summary element found");
+    }
 }
