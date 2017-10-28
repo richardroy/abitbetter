86c45bfbeb4cafbe0eff5f02a75688a82f863ad4
richardroy <richardroygibson@gmail.com>
2017-10-28 21:00:26 +1100

    it now adds the file list to the left menu, if you hover it shows the full path, will show back one dir

--- content.js
+++ content.js
@@ -1,23 +1,45 @@
 window.addEventListener ("load", myMain, false);
 
+    //classes exist?
+const fileSummaryRowClass = "iterable-item file file-modified"    
+const fileListLinkClass = "commit-files-summary--filename";
+const commitFilesSummaryId = "commit-files-summary";
+const diffStatsDivClass = "commit-file-diff-stats";
+const linesRemovedClass = "lines-removed";
+const linesAddedClass = "lines-added";    
+const navigationParentClass = "adg3-navigation";
+
 function myMain (evt) {
     insertDiffTotal();
+    addLeftFileList();
+}
+
+function addLeftFileList() {
+    const summaryElement = window.document.getElementById(commitFilesSummaryId);
+    if(summaryElement){
+        const linkListName = summaryElement.getElementsByClassName(fileListLinkClass);
+        const linkList = linkListName;        
+        const navigationSubList = document.getElementById("adg3-navigation").firstChild.firstChild.firstChild.firstChild.childNodes[1].firstChild.childNodes[2].firstChild.firstChild;
+        
+        for(let i = 0; i < linkList.length; i++){
+            let textContent = linkList[i].textContent;
+            const splitUrl = textContent.split("/");
+            let linkText = `${splitUrl[splitUrl.length-2]}/${splitUrl[splitUrl.length-1]}`;
+            linkList[i].textContent = linkText;
+            linkList[i].title = textContent;
+            navigationSubList.appendChild(linkList[i].cloneNode(true));                
+        }
+    } else {
+        console.warn("Delay: No summary element found");
+    }
 }
 
 function insertDiffTotal() {
     //enter here the action you want to do once loaded
     console.log("runChanges");
 
-    //classes exist?
-    const fileSummaryRowClass = "iterable-item file file-modified"    
-    const commitFilesSummaryId = "commit-files-summary";
-    const diffStatsDivClass = "commit-file-diff-stats";
-    const linesRemovedClass = "lines-removed";
-    const linesAddedClass = "lines-added";    
-
     const summaryElement = window.document.getElementById(commitFilesSummaryId);
     if(summaryElement){
-
         let removed  = summaryElement.getElementsByClassName(linesRemovedClass);
         let totalRemoved = 0;
         for (let i = 0; i < removed.length; i++) {
